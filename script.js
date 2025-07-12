93b08d2da74aec123f4c07257aff258c61dfdc62
Farhan Hakim <farhannulhakim782005@gmail.com>
2025-07-12 23:50:14 +0700

    update tampilan

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const navToggle = document.querySelector('.nav-toggle');
-      const navUl = document.querySelector('nav ul');
+const navUl = document.querySelector('nav ul');
 
-      navToggle.addEventListener('click', () => {
-        navUl.classList.toggle('nav-open');
-        navToggle.classList.toggle('active');
-      });
+navToggle.addEventListener('click', () => {
+  navUl.classList.toggle('nav-open');
+  navToggle.classList.toggle('active');
+});
 
-      // Optional: close menu when link clicked (mobile UX)
-      document.querySelectorAll('nav ul li a').forEach(link => {
-        link.addEventListener('click', () => {
-          if(window.innerWidth <= 700){
-            navUl.classList.remove('nav-open');
-            navToggle.classList.remove('active');
-          }
-        });
-      });
+// Optional: close menu when link clicked (mobile UX)
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', () => {
+    if (window.innerWidth <= 700) {
+      navUl.classList.remove('nav-open');
+      navToggle.classList.remove('active');
+    }
+  });
+});
+
+
+
+
+
